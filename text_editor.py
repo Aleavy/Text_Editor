831153cd741d577f40470a83bdd29fc979936af4
ESCOBAR MARIN ENZO DAVID <ENZOES@ITAIPU.GOV.PY>
2025-01-23 15:08:52 -0300

    Added highlight, btn for increasing and decreasing font size, logic to remove and overwrite highlight. There is left to do a method to store the tags inside the Text widget and then parse it correctly (aparently this can be done with the dump method)

--- text_editor.py
+++ text_editor.py
@@ -2,8 +2,8 @@ import tkinter as tk
 from tkinter.filedialog import askopenfile, asksaveasfile
 
 def save_as_file():
-    content = text_wg.get(1.0, "end-1c")
-
+    content = text_wg.dump(1.0, "end-1c")
+    print(content)
     files = [("All files", "*"),
             ("Python files", ".py"),
             ("Text files", ".txt")] 
@@ -19,6 +19,40 @@ def open_file():
 
     text_wg.replace(1.0, "end", file.read())
 
+def highlight_text(color:str): 
+        
+    # if no text is selected then tk.TclError exception occurs 
+    try: 
+        start, finish = text_wg.index("sel.first"), text_wg.index("sel.last")
+        tags = text_wg.tag_names(start)
+        if len(tags) > 1:
+            if f"{color}" in tags:
+                text_wg.tag_remove(f"{color}", start, finish)
+                return
+            for tag in tags:
+                print(tag)
+                if tag == "sel" or tag == "more":
+                    continue
+                text_wg.tag_remove(tag, start, finish)
+            text_wg.tag_add(f"{color}", "sel.first", "sel.last")      
+        else:
+            text_wg.tag_add(f"{color}", "sel.first", "sel.last")         
+    except tk.TclError: 
+        pass
+
+
+def increase_font():
+    try:
+        text_wg.tag_add("more", "sel.first", "sel.last")
+    except tk.TclError:
+        pass
+
+def decrease_font():
+    try:
+        text_wg.tag_remove("more", "sel.first", "sel.last")
+    except tk.TclError:
+        pass
+
 main_window = tk.Tk()
 main_window.title("Amazing Text Editor")
 
@@ -27,23 +61,37 @@ tools = tk.Frame(main_window, bg="black", border=4, relief="raised",height=400, 
 text_frame = tk.Frame(main_window)
 
 scrollbar = tk.Scrollbar(text_frame)
-text_wg = tk.Text(text_frame, border=4, font=("Arial", 20), yscrollcommand=scrollbar.set)
+text_wg = tk.Text(text_frame, border=4, font=("Arial", 20), yscrollcommand=scrollbar.set, undo = True)
 scrollbar.config(command=text_wg.yview)
 save_btn = tk.Button(tools, text="Save", relief="solid", font=("Arial", 10), command=save_as_file)
 open_btn = tk.Button(tools, text="Open", relief="solid", font=("Arial", 10), command=open_file)
-
+header_btn = tk.Button(tools, text="Header", relief="solid", command=increase_font)
+body_btn = tk.Button(tools, text="Body", relief="solid", command=decrease_font)
 
 tools.grid_propagate(False)
 tools.grid(column=0, row=0, sticky=("NSEW"))
 
 save_btn.grid(column=0, row=0, sticky=("NEW"))
 open_btn.grid(column=1, row=0, sticky=("NEW"))
+header_btn.grid(column=0, row=1, columnspan=1, sticky=("NEW"))
+body_btn.grid(column=1, row=1, columnspan=1, sticky=("NEW"))
 
 text_frame.grid(column=1, row=0, sticky=("NSEW"))
 text_wg.grid(column=0, row=0, sticky=("NSEW"))
+
 scrollbar.grid(column=1, row=0, sticky=("NS"))
 
+text_wg.tag_configure("red", background="red", foreground="white") 
+text_wg.tag_configure("blue", background="blue", foreground="white") 
+text_wg.tag_configure("yellow", background="yellow", foreground="black") 
+font_size = text_wg.config("font")[-1].split(" ")
+print(font_size)
+text_wg.tag_configure("more", font= (font_size[0], int(font_size[1]) + 10)) 
+
 main_window.bind("<Control-d>", lambda x: main_window.destroy())
+main_window.bind("<Control-r>", lambda x: highlight_text("red"))
+main_window.bind("<Control-q>", lambda x: highlight_text("yellow"))
+main_window.bind("<Control-f>", lambda x: highlight_text("blue"))
 
 
 main_window.grid_columnconfigure(0, weight=1)
